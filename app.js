daf5ebbc2dd810f9dc780301bc31ebeeaf64e0a5
chopper011 <dexterorozco13@gmail.com>
2019-11-20 15:31:15 +0100

    problem not found

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Lin: iqoIN5sqmJx6o2pt@note-3zsub.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+ const collection = client.db("test").collection("devices");
+ console.log("Databas connected!");
+ client.close();
+});
+
 var express = require('express');
 var app = express();
 
